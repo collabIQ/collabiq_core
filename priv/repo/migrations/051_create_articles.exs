22339c2ade7177dbfb83ab38487ad1e5ea6948b6
Justin Coker <justin.coker@outlook.com>
2019-01-21 20:07:47 -0600

    Updated articles migration to include content

--- priv/repo/migrations/051_create_articles.exs
+++ priv/repo/migrations/051_create_articles.exs
@@ -6,6 +6,7 @@ defmodule Core.Repo.Migrations.CreateArticles do
       add :id, :binary_id, primary_key: true
       add :tenant_id, references(:tenants, on_delete: :delete_all, type: :binary_id), null: false
       add :workspace_id, references(:workspaces, on_delete: :delete_all, type: :binary_id), null: false
+      add :content, :text, null: false
       add :name, :string, null: false
       add :status, :string
       add :type, :string
@@ -15,7 +16,7 @@ defmodule Core.Repo.Migrations.CreateArticles do
     end
 
     create index(:articles, [:tenant_id])
-    create index(:workspaces, [:workspace_id])
+    create index(:articles, [:workspace_id])
     create index(:articles, ["(lower(name))"], name: :articles_name_index)
   end
 end
