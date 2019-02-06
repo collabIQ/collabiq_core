5780da53143bbe5642644fbb45b3e63935d36624
Justin Coker <justin.coker@outlook.com>
2019-02-05 21:41:23 -0600

    Moved the color validation and changeset functions to the Color module.

--- lib/core/color.ex
+++ lib/core/color.ex
@@ -1,5 +1,29 @@
 defmodule Core.Color do
+  import Ecto.Changeset
+
   @chars "abcdef0123456789"
+
+  def changeset(%{params: %{"color" => color}} = changeset)
+      when is_binary(color) and color != "" do
+    with {:ok, _color} <- validate(color) do
+      changeset
+    else
+      _ ->
+        changeset
+        |> add_error(:color, "is invalid")
+    end
+  end
+
+  def changeset(%{data: %{color: color}} = changeset) when is_binary(color) and color != "" do
+    changeset
+  end
+
+  def changeset(changeset) do
+    color = random()
+
+    changeset
+    |> put_change(:color, color)
+  end
 
   def random() do
     chars =
@@ -8,9 +32,21 @@ defmodule Core.Color do
 
     hex =
       1..6
-      |> Enum.map(fn(_x) -> Enum.random(chars) end)
+      |> Enum.map(fn _x -> Enum.random(chars) end)
       |> Enum.join("")
 
     "#" <> hex
   end
+
+  def validate(hex) when is_binary(hex) do
+    regex = ~r"^#([A-Fa-f0-9]{6})\z"
+
+    if hex =~ regex do
+      {:ok, hex}
+    else
+      :error
+    end
+  end
+
+  def validate(_), do: :error
 end
